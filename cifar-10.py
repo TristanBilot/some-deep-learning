75a372b65ddc5a3fe97f8050529df69309516ba0
Tristan Bilot <tristan_bilot@carrefour.com>
2021-03-10 23:13:54 +0100

    improve accuracy with more filters deeper in CNN

--- cifar-10.py
+++ cifar-10.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import visualkeras
 
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
 
@@ -12,10 +13,10 @@ test_images = test_images / 255.0
 model = keras.models.Sequential()
 model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(keras.layers.ZeroPadding2D())
-model.add(keras.layers.MaxPool2D((2, 2)))
-model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(keras.layers.MaxPooling2D((2, 2)))
 model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(keras.layers.MaxPooling2D((2, 2)))
+model.add(keras.layers.Conv2D(128, (3, 3), activation='relu'))
 
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(64, activation='relu'))
@@ -23,10 +24,11 @@ model.add(keras.layers.Dense(10))
 
 # Train
 model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'], optimizer='adam')
-model.fit(train_images, train_labels, epochs=4, batch_size=32, validation_data=(test_images, test_labels))
+model.fit(train_images, train_labels, epochs=1, batch_size=32, validation_data=(test_images, test_labels))
 
 # Test
 loss, accuracy = model.evaluate(test_images, test_labels)
 
+visualkeras.layered_view(model).show()
 print(accuracy)
 print(loss)
